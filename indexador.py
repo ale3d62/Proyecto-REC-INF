d8538ba69f34c9d676eafb59db2ba448100e59c3
ale3d62 <ale3d62@gmail.com>
2023-12-26 19:04:28 +0100

    Arreglado bug
    Arreglado bug donde el TF se calculaba varias veces y las apariciones se contaban varias veces cuando un termino sale más de una vez en un documento

--- indexador.py
+++ indexador.py
@@ -50,30 +50,30 @@ if __name__ == "__main__":
 
     for iDocumento, documento in enumerate(documentos):
         if(iDocumento%1000 == 0): 
-            print("Documentos procesados: "+iDocumento+"/"+nDocumentos)
+            print("Documentos procesados: "+str(iDocumento+1)+"/"+str(nDocumentos))
 
         terminosDocumento = obtenerTerminos(documento)
-        nTerminosPrevio = len(terminos)
-        terminos.update(terminosDocumento)
+        setTerminosDocumento = set(terminosDocumento)
+        terminos.update(setTerminosDocumento) #añadir terminos al set de terminos global
 
-        for termino in terminosDocumento:
+        for termino in setTerminosDocumento:
             #Si el termino es nuevo, inicializamos su fila en la matriz para poder trabajar
             if(not (termino in apariciones)):
                 TF[termino] = ([0]*nDocumentos) 
                 apariciones[termino] = 0
 
+            #Calculamos TF
             nApariciones = terminosDocumento.count(termino)
-            TF[termino][iDocumento] = (1+log2(nApariciones)) 
+            TF[termino][iDocumento] = (1+log2(nApariciones))
             apariciones[termino] += 1
 
 
     for termino in terminos:
         IDF.append(log2(nDocumentos/apariciones[termino]))
 
-
     nTerminos = len(terminos)
     TFIDF = [[0]*nDocumentos]*nTerminos
-    print(terminos)
+
     for i, termino in enumerate(terminos):
         for j in range(nDocumentos):
             TFIDF[i][j] = TF[termino][j] * IDF[i]
